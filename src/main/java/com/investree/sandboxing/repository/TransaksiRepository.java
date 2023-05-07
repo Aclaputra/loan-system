083b7097642fce5b635be27214d3cfc1939a98f0
Aclaputra <muhammadacla22@gmail.com>
2023-05-07 17:26:10 +0700

    initial commit

--- src/main/java/com/investree/sandboxing/repository/TransaksiRepository.java
+++ src/main/java/com/investree/sandboxing/repository/TransaksiRepository.java
@@ -3,13 +3,18 @@ package com.investree.sandboxing.repository;
 import com.investree.sandboxing.model.Transaksi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransaksiRepository extends PagingAndSortingRepository<Transaksi, Long> {
+public interface TransaksiRepository extends
+        PagingAndSortingRepository<Transaksi, Long>,
+        JpaRepository<Transaksi, Long>
+{
+
     @Query("select t from Transaksi t WHERE t.id = :id")
     public Transaksi getById(@Param("id") Long id);
     @Query("select t from Transaksi t")
--- src/main/java/com/investree/sandboxing/view/impl/TransaksiPaymentImple.java
+++ src/main/java/com/investree/sandboxing/view/impl/TransaksiPaymentImple.java
@@ -1,18 +1,47 @@
 package com.investree.sandboxing.view.impl;
 
 import com.investree.sandboxing.model.Transaksi;
+import com.investree.sandboxing.repository.TransaksiRepository;
 import com.investree.sandboxing.view.TransaksiService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TransaksiPaymentImple implements TransaksiService {
+    @Autowired
+    public TransaksiRepository transaksiRepository;
     @Override
     public Map save(Transaksi obj) {
-        return null;
+        Map map = new HashMap();
+        try {
+            Transaksi save = transaksiRepository.save(obj);
+            map.put("data", save);
+            map.put("code", "200");
+            map.put("status", "success");
+            return map;
+        } catch (Exception e) {
+            map.put("code", "500");
+            map.put("status", "failed");
+            return map;
+        }
     }
 
     @Override
     public Map updateStatus(Transaksi obj) {
-        return null;
+        Map map = new HashMap();
+        try {
+            Transaksi update = transaksiRepository.getById(obj.getId());
+            update.setStatus(obj.getStatus());
+            Transaksi doSave = transaksiRepository.save(update);
+            map.put("data",doSave);
+            map.put("code", "200");
+            map.put("status", "success");
+        } catch (Exception e) {
+            map.put("code", "500");
+            map.put("status", "failed");
+            return map;
+        }
+        return map;
     }
 }
